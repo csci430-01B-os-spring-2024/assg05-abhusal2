049d0d4a9f7a6f71450065247201ee1adf061a24
abhusal2 <abhusal2@leomail.tamuc.edu>
2024-04-25 18:50:16 +0000

    Task 5
    enabled the complete FCFS simulation by uncommented prior code which was already implemented in

--- src/SchedulingSystem.cpp
+++ src/SchedulingSystem.cpp
@@ -672,7 +672,7 @@ void SchedulingSystem::runSimulation(bool verbose)
   // to make scheduling decisions.  We keep running the simulation until
   // all processes in the process table are done
   string schedule = "";
-  /*
+  
   while (not allProcessesDone())
   {
     //cout << "runSimulation()> systemTime: " << systemTime << endl;
@@ -698,7 +698,7 @@ void SchedulingSystem::runSimulation(bool verbose)
     // is up to date for scheduling policies to use
     updateProcessStatistics();
   }
-  */
+  
 
   // Display scheduling simulation results if asked too
   if (verbose)
